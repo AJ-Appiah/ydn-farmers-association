891e12b3b92264625c42bceebef0c53df9fb8fba
margaret boye <margaretboye8@gmail.com>
2025-07-25 13:24:03 +0000

    feat(SectionHeaders): make margin-bottom customizable
    - Import pt serif font style for subtitle
    - Default to mb-10 if no value provided

--- src/components/sectionHeaders.jsx
+++ src/components/sectionHeaders.jsx
@@ -1,21 +1,19 @@
 import React from "react";
 
-const SectionHeaders = ({ subTitle, title }) => {
+const SectionHeaders = ({ subTitle, title, mb = "mb-10" }) => {
   return (
-    <>
-      <header>
-        <p className="text-base text-[var(--color-ydn-green)] italic">
-          {subTitle}
-        </p>
+    <header className={`${mb}`}>
+      <p className="text-[18px] pt-serif-italic capitalize text-[var(--color-ydn-green)] italic">
+        {subTitle}
+      </p>
 
-        <h2 className="text-2xl roboto-slab relative uppercase font-bold text-[var(--color-heading)] ">
-          <span className="relative pr-2.5 ">
-            {title}
-            <div className="w-12 h-0.5 bg-[#99c802] absolute -right-11 top-6"></div>
-          </span>
-        </h2>
-      </header>
-    </>
+      <h2 className="text-2xl roboto-slab--700 relative uppercase font-bold text-[var(--color-heading)] ">
+        <span className="relative pr-2.5 ">
+          {title}
+          <div className="w-12 h-0.5 bg-[#99c802] absolute -right-11 top-6"></div>
+        </span>
+      </h2>
+    </header>
   );
 };
 
