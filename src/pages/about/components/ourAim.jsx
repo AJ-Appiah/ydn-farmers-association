84dffe1f80bc6931f3053f23cab386b1400c5825
margaret boye <margaretboye8@gmail.com>
2025-07-28 17:38:28 +0000

    feat(OurAim): add our aim section to the about page

--- src/pages/about/components/ourAim.jsx
+++ src/pages/about/components/ourAim.jsx
@@ -2,11 +2,13 @@ import React from "react";
 
 const OurAim = () => {
   return (
-    <section className=" ">
+    <section className="py-[73px] bg-[#F5F4F4]">
       <div className="container ">
-        <div className="py-[73px]">
-          <h2>Our Mission</h2>
-          <p>
+        <div className="px-5">
+          <h2 className="roboto-slab--700 relative uppercase font-bold text-[var(--color-heading)] text-[28px] mb-4">
+            Our Mission
+          </h2>
+          <p className="text-[#8e8e8d] text-[14px] leading-[26px] ">
             Our mission is to develop beneficial programs favorable for
             increasing agricultural production, processing and, marketing
             through building and strengthening the capacities of farmers in
