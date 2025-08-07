db569bc897b1b91af361890a40556f24d65ab99c
margaret boye <margaretboye8@gmail.com>
2025-08-07 08:47:20 +0000

    feat(AboutHero): Add reusable component for hero section

--- src/components/pagesHero.jsx
+++ src/components/pagesHero.jsx
@@ -4,16 +4,19 @@ import { Link } from "react-router-dom";
 const PagesHero = ({ pageName, pageTitle, pageRoute }) => {
   return (
     <section
-      className="text-white flex flex-col items-center pb-[52px] pt-[67px] bg-center h-[40vh]"
+      className="text-white flex flex-col items-center pb-[52px] pt-[67px] bg-center bg-cover bg-no-repeat"
       style={{ backgroundImage: `url(${AboutHeroImage})` }}
     >
-      <h1 className="mb-2 font-bold">{pageTitle} </h1>
+      <h1 className="mb-2 roboto-slab--700 uppercase">{pageTitle} </h1>
 
-      <ul className="flex gap-x-1 text-white">
+      <ul className="flex gap-x-1 text-white text-[12px] font-semibold">
         <li className="pr-[3px]">
           <Link to={pageRoute}>HOME </Link>
         </li>
-        <li>{pageName}</li>
+        <li className="uppercase">
+          <span className="px-1">/ </span>
+          {pageName}
+        </li>
       </ul>
     </section>
   );
