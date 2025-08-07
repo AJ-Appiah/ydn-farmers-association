af8b9f1c62200fbad4b1386d4f438de20c8e992a
margaret boye <margaretboye8@gmail.com>
2025-08-07 08:24:40 +0000

    feat(ReusableConponents): add reusable component for hero section of pages

---
+++ src/components/pagesHero.jsx
@@ -0,0 +1,22 @@
+import AboutHeroImage from "../assets/images/about-hero.jpg";
+import { Link } from "react-router-dom";
+
+const PagesHero = ({ pageName, pageTitle, pageRoute }) => {
+  return (
+    <section
+      className="text-white flex flex-col items-center pb-[52px] pt-[67px] bg-center"
+      style={{ backgroundImage: `url(${AboutHeroImage})` }}
+    >
+      <h1 className="mb-2 font-bold">{pageTitle} </h1>
+
+      <ul className="flex gap-x-1 text-white">
+        <li className="pr-[3px]">
+          <Link to={pageRoute}>HOME </Link>
+        </li>
+        <li>{pageName}</li>
+      </ul>
+    </section>
+  );
+};
+
+export default PagesHero;
